ef69806bdc0413faed1c8c2753b9b337bca93214
Carlos Lantigua <clantigua954@gmail.com>
2019-03-25 09:43:10 -0400

    initialize project

---
+++ shell.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+int main(void)
+{
+    int sockt;
+    int port = "4444";
+    struct shell
+    {
+        /* data */
+    };
+}
